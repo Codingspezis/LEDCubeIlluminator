13403833278b300e03d810185335c02884dcb64c
Robert Salamon <rbrtslmn@gmail.com>
2014-03-17 21:37:01 +0100

    Added windows support

--- communicator.c
+++ communicator.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include <stdint.h>
 
+#ifdef unix
+
+/*
+ * ### UNIX FUNCTIONS ###
+ * ######################
+ */
+
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
@@ -22,3 +29,59 @@ int initClient(char *addr, int port) {
 int sendState(uint8_t *data) {
 	return send(g_sock, data, 16, 0);
 }
+
+#else
+
+/*
+ * ### WINDOWS FUNCTIONS ###
+ * #########################
+ */
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+SOCKET ConnectSocket = INVALID_SOCKET;
+
+int initClient(char *addr, int port) {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2,2), &wsaData)) {
+        return -1;
+    }
+    struct addrinfo *ptr = NULL;
+    struct addrinfo hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    char buffer[6];
+    sprintf(buffer, "%i", port);
+    if(getaddrinfo(addr, buffer, &hints, &ptr)) {
+        WSACleanup();
+        return -1;
+    }
+    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+    if (ConnectSocket == INVALID_SOCKET) {
+        freeaddrinfo(ptr);
+        WSACleanup();
+        return -1;
+    }
+    if(SOCKET_ERROR == connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen)) {
+        closesocket(ConnectSocket);
+        ConnectSocket = INVALID_SOCKET;
+        return -1;
+    }
+    if (ConnectSocket == INVALID_SOCKET) {
+        freeaddrinfo(ptr);
+        WSACleanup();
+        return -1;
+    }
+    return 0;
+}
+
+int sendState(uint8_t *data) {
+    if(SOCKET_ERROR != send(ConnectSocket, data, 16, 0))
+        return 16;
+    return -1;
+}
+
+#endif
--- main.c
+++ main.c
@@ -6,25 +6,25 @@
 #include "effects.h"
 
 int main(int argc, char **argv) {
-	// check arguments
-	if(argc < 3) {
-		printf("usage: %s addr port\n", *argv);
-		return 0;
-	}
-	// connect to server
-	if(initClient(argv[1], atoi(argv[2])) < 0) {
-		printf("could not set up connection\n");
-		return -1;
-	}
-	// illumination loop
-	for(;;) {
-		textRotateEffectLoop("LED", 100, 1);
-		randomEffectLoop(100, 20);
-		snakeEffectLoop(10, 100, 50);
-		textXEffectLoop("WWW.CODINGSPEZIS.COM", 75, 1);
-		rotateXEffectLoop(100, 1);
-		rotateYEffectLoop(100, 1);
-		rotateZEffectLoop(100, 1);
-	}
-	return 0;
+    // check arguments
+    if(argc < 3) {
+        printf("usage: %s addr port\n", *argv);
+        return 0;
+    }
+    // connect to server
+    if(initClient(argv[1], atoi(argv[2])) < 0) {
+        printf("could not set up connection\n");
+        return -1;
+    }
+    // illumination loop
+    for(;;) {
+        textRotateEffectLoop("LED", 100, 1);
+        randomEffectLoop(100, 20);
+        snakeEffectLoop(10, 100, 50);
+        textXEffectLoop("WWW.CODINGSPEZIS.COM", 75, 1);
+        rotateXEffectLoop(100, 1);
+        rotateYEffectLoop(100, 1);
+        rotateZEffectLoop(100, 1);
+    }
+    return 0;
 }
